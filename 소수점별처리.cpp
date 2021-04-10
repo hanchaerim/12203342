29a58d35ae07d9df4338b17fb50a1c2f04142810
hanchaerim <lunatheworld@naver.com>
2021-04-10 21:22:56 +0900

    3

--- 소수점별처리.cpp
+++ 소수점별처리.cpp
@@ -5,9 +5,15 @@ using namespace std;
 int main(){
     double x = 1237234.1235;
     cout << fixed << setprecision(2) << showpos << setfill('*');
+    // setprecision(2) = 소수점을 두 자리까지만 사용
+    // showpos = 양수/음수 부호 둘 다 출력
+    // sethill(*) = 빈칸을 *로 채운다
 
     cout << setw(15) << left << x << endl;
     cout << setw(15) << internal << x << endl;
     cout << setw(15) << right << x;
+    // left = 숫자를 왼쪽 정렬
+    // internal = 부호 왼쪽,    , 오른쪽 숫자
+    // right = 부호 오른쪽 정렬
     return 0;
 }
