8cc8bba697e7bbd3067b74e73177868c260f911e
hanchaerim <lunatheworld@naver.com>
2021-04-10 16:48:58 +0900

    2

--- 리터럴값사용.cpp
+++ 리터럴값사용.cpp
@@ -4,10 +4,12 @@ using namespace std;
 int main(){
     int x;
     unsigned long int y;
-    // unsigned = 부호 없이 만들고 싶을 때
-    
+    // long int = int의 범위를 넘는 경우; 뒤에 'ㅣ' 이나 'L' 시용 
+    // unsigned long int= 부호 없이 만들고 싶을 때; 뒤에 'u'나 'U' 사용
+
     x= 1456;
     y= -14567;
+    // long int 인데 음수 값이라 출력 결과 오류가 날 것
 
     cout << x << endl;
     cout << y << endl;
